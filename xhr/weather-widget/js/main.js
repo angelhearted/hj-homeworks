03f2793558df94da7c864f60758a92658c2b51cb
Nikita Skrebets <angelhearted@ya.ru>
2018-06-25 23:53:30 +0300

    Added async load of request

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,7 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
-request.send();
-if (request.status === 200) {
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather');
+request.addEventListener('load', event => {
   const response = JSON.parse(request.responseText);
   setData(response);
-}
+});
+request.send();
