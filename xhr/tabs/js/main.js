4b87c98a1de2dc5bc46f71b5cfad52d4fdd2b0bf
Nikita Skrebets <angelhearted@ya.ru>
2018-06-26 02:05:52 +0300

    fix tab opened by default

--- xhr/tabs/js/main.js
+++ xhr/tabs/js/main.js
@@ -2,10 +2,14 @@ const menu = document.querySelector('nav');
 const content = document.querySelector('#content');
 const preloader = document.querySelector('#preloader');
 
-menu.addEventListener('click', event => {
+menu.addEventListener('click', clickMenuLink);
+
+function clickMenuLink(event) {
+  console.log(event);
   const a = event.target;
   if (a.tagName !== 'A' || a.classList.contains('active')) return;
-  
+  console.log(1);
+
   event.preventDefault();
   menu.querySelector('a.active').classList.remove('active');
   a.classList.add('active');
@@ -13,12 +17,19 @@ menu.addEventListener('click', event => {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', a.href);
 
-  xhr.addEventListener('loadstart', event => {
+  xhr.addEventListener('loadstart', () => {
     preloader.classList.remove('hidden');
   });
-  xhr.addEventListener('load', event => {
+  xhr.addEventListener('load', () => {
     preloader.classList.add('hidden');
     content.innerHTML = xhr.responseText;
   });
   xhr.send();
-});
+}
+
+// set email tab as default
+const event = {
+  target: menu.querySelector('a'),
+  preventDefault: () => {}
+};
+clickMenuLink(event);
