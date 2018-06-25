b41fb16d768a0fbe2a244da0a350c07d2926cb65
Nikita Skrebets <angelhearted@ya.ru>
2018-06-25 20:58:03 +0300

    Created Cart class, implemented addItem

--- dom/shop/js/price-format.js
+++ dom/shop/js/price-format.js
@@ -1,3 +1,34 @@
 function getPriceFormatted(value) {
   return  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
+
+const cart = new Cart();
+
+function Cart() {
+  const container = document.querySelector('#container');
+  const countEl = container.querySelector('#cart-count');
+  const sumEl = container.querySelector('#cart-total-price');
+  let count = 0;
+  let sum = 0;
+
+  container.addEventListener('click', event => 
+    event.target.tagName === 'BUTTON' && this.addItem(event.target.parentElement)
+  );
+
+  this.addItem = prodParent => {
+    const product = parseProduct(prodParent);
+
+    sum += product.price;
+    sumEl.textContent = getPriceFormatted(sum);
+
+    count++;
+    countEl.textContent = count;
+  }
+
+  function parseProduct(prodParent) {
+    const name = prodParent.querySelector('h3').textContent;
+    // don't need it here, but created anyway
+    const price = +prodParent.querySelector('button').dataset.price;
+    return { name: name, price: price };
+  }
+}
